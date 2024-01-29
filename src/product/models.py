7e9d81c80f18dfb91cd76dee3f522cb0925ccc4f
hmursaleen <habibulmursaleen@gmail.com>
2024-01-29 11:32:22 +0600

    edited models according to DFD

--- src/product/models.py
+++ src/product/models.py
@@ -4,26 +4,35 @@ from config.g_model import TimeStampMixin
 
 # Create your models here.
 class Variant(TimeStampMixin):
-    title = models.CharField(max_length=40, unique=True)
+    title = models.CharField(max_length=255, unique=True)
     description = models.TextField()
-    active = models.BooleanField(default=True)
+    #active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Product(TimeStampMixin):
     title = models.CharField(max_length=255)
     sku = models.SlugField(max_length=255, unique=True)
     description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class ProductImage(TimeStampMixin):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     file_path = models.URLField()
+    thumbnail = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class ProductVariant(TimeStampMixin):
     variant_title = models.CharField(max_length=255)
     variant = models.ForeignKey(Variant, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class ProductVariantPrice(TimeStampMixin):
@@ -33,6 +42,8 @@ class ProductVariantPrice(TimeStampMixin):
                                             related_name='product_variant_two')
     product_variant_three = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, null=True,
                                               related_name='product_variant_three')
-    price = models.FloatField()
-    stock = models.FloatField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField(default=0)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
